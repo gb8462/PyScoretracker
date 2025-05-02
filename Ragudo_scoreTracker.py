861020d8d61e419f74ca673d46b518e832b3f408
gb8462 <planetes8462@gmail.com>
2025-05-01 19:29:39 -0700

    update

--- Ragudo_scoreTracker.py
+++ Ragudo_scoreTracker.py
@@ -2,7 +2,22 @@ import tkinter as tk
 
 main = tk.Tk()
 main.title('ScoreTracker')
-main.geometry('500x570')
+main.geometry('500x560')
 main.config(bg='#e8e2e1')
+
+title = tk.Label(main, text='Score Tracker', bg='#e8e2e1')
+title.pack()
+
+# =================================================
+frame = tk.Frame(main, bg='white')
+frame.pack()
+
+studentName = tk.Label(frame, text='Student Name', bg='white')
+studentName.grid(row = 1, column = 0)
+
+score = tk.Label(frame, text='Student Score', bg='white')
+score.grid(row = 1, column = 1)
+# =================================================
+
 
 main.mainloop()
