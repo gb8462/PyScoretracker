53ecb1a976ade6bdc252b4ebf7b5298db5d26ad7
gb8462 <planetes8462@gmail.com>
2025-05-02 19:22:52 -0700

    update

--- Ragudo_scoreTracker.py
+++ Ragudo_scoreTracker.py
@@ -5,8 +5,8 @@ main = tk.Tk()
 main.title('ScoreTracker')
 main.geometry('750x400')
 main.config(bg='#e4eaeb')
-# ========== Functions Here ==========
 
+# ========== Functions Here ==========
 def initialize_database():
     if not os.path.exists("student_scores.xlsx"):
         workbook = openpyxl.Workbook()
@@ -31,14 +31,14 @@ title.pack(pady=(20,0))
 studentName = tk.Label(frame1, text='Student Name', font=('Arial',12),bg='#e4eaeb')
 studentName.pack(pady=(40,2), padx=20)
 
-entry = tk.Entry(frame1, width=20,font=('Arial',11))
-entry.pack(padx=20)
+name_entry = tk.Entry(frame1, width=20,font=('Arial',11))
+name_entry.pack(padx=20)
 
 score = tk.Label(frame1, text='Student Score', font=('Arial',12), bg='#e4eaeb')
 score.pack(pady=(10,2), padx=20)
 
-entry = tk.Entry(frame1, width=20, font=('Arial',11))
-entry.pack(padx=20)
+score_entry = tk.Entry(frame1, width=20, font=('Arial',11))
+score_entry.pack(padx=20)
 
 submit = tk.Button(frame1, text='submit', command=Submit)
 submit.pack(pady=20, padx=20)
